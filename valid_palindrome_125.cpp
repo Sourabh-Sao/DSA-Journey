372d4af5812403686296aa244bbbb758bf068fa3
Sourabh Sao <133841512+Sourabh-Sao@users.noreply.github.com>
2025-01-13 19:20:23 +0530

    Add files via upload

---
+++ valid_palindrome_125.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ bool isAlphanumeric(char ch)
+{
+    if ((ch>='0' && ch<='9') || 
+    (tolower(ch)>='a' && tolower(ch)<='z'))
+    {
+        return true;
+    }
+    return false;
+    
+}
+    bool isPalindrome(string s) {
+        int n = s.size();
+    int st = 0, end = n - 1;
+
+    while (st < end)
+    {
+        if (!isAlphanumeric(s[st])) //checking if start pointer the characters are alphanumeric or not
+        {
+            st++;
+            continue;
+        }
+        if (!isAlphanumeric(s[end])) //checking if end pointer the characters are alphanumeric or not
+        {
+            end--;
+            continue;
+        }
+        if (tolower(s[st]) != tolower(s[end]))  //checking if the characters are same or not
+        {
+            return false;  //if not same then return false
+        }
+        st++;
+        end--;
+    }
+    return true; 
+    }
+
+
+int main(){
+    string s="racecar";
+    cout<<isPalindrome(s);
+    return 0;
+}
