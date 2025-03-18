78ff6862e35b619d303da159c3f48a868fe6257f
Sourabh Sao <133841512+Sourabh-Sao@users.noreply.github.com>
2025-03-18 22:52:58 +0530

    Update Linked_List_Cycle-II_142.cpp

--- Linked_List_Cycle-II_142.cpp
+++ Linked_List_Cycle-II_142.cpp
@@ -1,3 +1,4 @@
+// below code is to get the node from where the cycle starts
 ListNode *detectCycle(ListNode *head) {
     ListNode *slow=head;
     ListNode *fast=head;
@@ -19,5 +20,33 @@ ListNode *detectCycle(ListNode *head) {
         slow=slow->next;
         fast=fast->next;
     }
+    return slow;
+}
+
+//The below code is to remove the cycle
+ListNode *detectCycle(ListNode *head) {
+    ListNode *slow=head;
+    ListNode *fast=head;
+    bool isCycle=false;
+
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            isCycle=true;
+            break;
+        }
+    }
+    if(!isCycle){
+        return NULL;
+    }
+    slow=head;
+    ListNode *prev=NULL  
+    while(slow!=fast){
+        slow=slow->next;
+        prev=fast; 
+        fast=fast->next;
+    }
+     prev->next=NULL;
     return slow;
 }
