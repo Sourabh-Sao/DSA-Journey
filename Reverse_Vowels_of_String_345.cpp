829cf54e4c40f2ee65dca2a7ae7c65b2c980314b
Sourabh Sao <133841512+Sourabh-Sao@users.noreply.github.com>
2025-03-22 23:11:49 +0530

    Add files via upload

---
+++ Reverse_Vowels_of_String_345.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isVowel(char ch){
+    ch=tolower(ch);
+    return ch=='a'||ch=='e' || ch=='i' || ch=='o' || ch=='u';
+}
+
+string reverseVowels(string s) {
+    int st=0,end=s.size();
+
+    while (st<end)
+    {
+        while (st<end && !isVowel(s[st])) st++;
+        while (st<end && !isVowel(s[end])) end--;
+        
+        if (st<end)
+        {
+            swap(s[st],s[end]);
+            st++,end--;
+        }
+        
+        
+    }
+    return s;
+}
+
+int main(){
+    string s="IceCreAm";
+    // AceCreIm
+    cout<<reverseVowels(s);
+    return 0;
+}
