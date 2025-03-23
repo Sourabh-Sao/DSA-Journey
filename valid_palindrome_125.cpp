c4088b0e5fe7dc75a92cba9df3c4360fbf1a9fa7
Sourabh Sao <133841512+Sourabh-Sao@users.noreply.github.com>
2025-03-23 15:11:27 +0530

    Update valid_palindrome_125.cpp

--- valid_palindrome_125.cpp
+++ valid_palindrome_125.cpp
@@ -1,43 +1,66 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
- bool isAlphanumeric(char ch)
-{
-    if ((ch>='0' && ch<='9') || 
-    (tolower(ch)>='a' && tolower(ch)<='z'))
-    {
-        return true;
-    }
-    return false;
+// Solution 1
+//  bool isAlphanumeric(char ch)
+// {
+//     if ((ch>='0' && ch<='9') || 
+//     (tolower(ch)>='a' && tolower(ch)<='z'))
+//     {
+//         return true;
+//     }
+//     return false;
     
-}
-    bool isPalindrome(string s) {
-        int n = s.size();
-    int st = 0, end = n - 1;
+// }
+//     bool isPalindrome(string s) {
+//         int n = s.size();
+//     int st = 0, end = n - 1;
 
-    while (st < end)
+//     while (st < end)
+//     {
+//         if (!isAlphanumeric(s[st])) //checking if start pointer the characters are alphanumeric or not
+//         {
+//             st++;
+//             continue;
+//         }
+//         if (!isAlphanumeric(s[end])) //checking if end pointer the characters are alphanumeric or not
+//         {
+//             end--;
+//             continue;
+//         }
+//         if (tolower(s[st]) != tolower(s[end]))  //checking if the characters are same or not
+//         {
+//             return false;  //if not same then return false
+//         }
+//         st++;
+//         end--;
+//     }
+//     return true; 
+//     }
+
+// Solution 2
+bool isPalindrome(string s){
+    int n=s.length();
+    int st=0,end=n-1;
+    while (st<end)
     {
-        if (!isAlphanumeric(s[st])) //checking if start pointer the characters are alphanumeric or not
-        {
+        if (!isalnum(s[st])){
             st++;
-            continue;
+            continue; 
         }
-        if (!isAlphanumeric(s[end])) //checking if end pointer the characters are alphanumeric or not
-        {
+        if (!isalnum(s[end])){
             end--;
             continue;
         }
-        if (tolower(s[st]) != tolower(s[end]))  //checking if the characters are same or not
-        {
-            return false;  //if not same then return false
+        
+        if(tolower(s[st])!=tolower(s[end])){
+            return false;
         }
-        st++;
-        end--;
+        st++,end--;
+        
     }
-    return true; 
-    }
-
+    return true;
+}
 
 int main(){
     string s="racecar";
