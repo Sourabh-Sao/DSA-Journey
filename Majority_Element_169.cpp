2117d2fc64e15f8cb1ab8877987128ab28d32bb4
Sourabh Sao <133841512+Sourabh-Sao@users.noreply.github.com>
2025-04-03 23:38:33 +0530

    Majority_Element_169.cpp
    Two pointer approach , i solve this on my own

--- Majority_Element_169.cpp
+++ Majority_Element_169.cpp
@@ -1,8 +1,30 @@
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
+
+// Solution using Two pointers
+// int majority(vector<int>&arr){
+//     int n=arr.size();
+//     if (n==1) return arr[0];
+//     sort(arr.begin(),arr.end());
+//     int i=0;
+//     for (int j = 1; j < n; j++)
+//     {
+//         int count=1;
+//         while (j<n && arr[i]==arr[j])
+//         {
+//             count++,j++;
+//         }
+//         if (count>n/2)
+//         {
+//             return arr[i];
+//         }
+//         i=j;
+//     }
+//     return -1;
+// }
+
 // most optimal approach
-// AKA Moore's voting Algorihtm
+// AKA Moore's voting Algorithm
 int majorityElement(vector<int> num){
     int freq=0,ans=0;
     for (int i = 0; i < num.size(); i++)
